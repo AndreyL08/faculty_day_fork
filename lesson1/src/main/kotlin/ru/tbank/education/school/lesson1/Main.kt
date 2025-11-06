c18aa9c74abd101d10848695eedde5cf52a998fe
AndreyL08 <lesnyhandrej746@gmail.com>
2025-11-06 10:49:58 +0300

    practice 1

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -2,4 +2,15 @@ package ru.tbank.education.school.lesson1
 
 fun main() {
     println("salam")
+    val str = """
+        1
+        2
+        3
+    """.trimIndent()
+    println(str)
+    var b = 1
+    b++
+    println(b)
+    val c = null
+    println(c)
 }
