fbe89785797b2e7321e3a60a2f8585a62da999e6
AndreyL08 <lesnyhandrej746@gmail.com>
2025-11-07 22:37:47 +0300

    calculate func

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,12 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    when (operation) {
+        OperationType.ADD -> return a + b
+        OperationType.SUBTRACT -> return a - b
+        OperationType.MULTIPLY -> return a * b
+        OperationType.DIVIDE -> if (b != 0.0) return a/b else return null
+    }
 }
 
 /**
@@ -14,5 +19,16 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val lst = this.split(" ")
+    val a = lst[0].toDoubleOrNull() ?: return null
+    val b = lst[2].toDoubleOrNull() ?: return null
+    val oper = lst[1]
+    val op: OperationType = when (oper) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+    return calculate(a, b, op)
 }
