be75b26cc4c4b4b3fd139b5c0e83f2d559e7021a
Andrey Lesnykh <lesnyhandrej746@gmail.com>
2026-03-07 22:42:06 +0300

    lesson 1

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,26 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Метод для вычисления простых арифметических операций.
- */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+
+fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
+    return when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b != 0.0) a / b else null
+    }
 }
 
-/**
- * Функция вычисления выражения, представленного строкой
- * @return результат вычисления строки или null, если вычисление невозможно
- * @sample "5 * 2".calculate()
- */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val (str_a, op, str_b) = this.split(" ")
+    val a = str_a.toDoubleOrNull()
+    val b = str_b.toDoubleOrNull()
+    if (a == null || b == null) return null
+    return when (op) {
+        "+" -> a + b
+        "-" -> a - b
+        "*" -> a * b
+        "/" -> if (b != 0.0) a / b else null
+        else -> null
+    }
 }
