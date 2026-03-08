4be2c9ac1fed6ce05b74c76dd96e8335251329ee
Andrey Lesnykh <lesnyhandrej746@gmail.com>
2026-03-09 00:51:32 +0300

    LibraryService

--- lesson8/src/main/kotlin/ru/tbank/education/school/lesson8/homework/library/LibraryService.kt
+++ lesson8/src/main/kotlin/ru/tbank/education/school/lesson8/homework/library/LibraryService.kt
@@ -1,37 +1,36 @@
 package ru.tbank.education.school.lesson8.homework.library
 
+import kotlin.math.max
+
 class LibraryService {
     private val books = mutableMapOf<String, Book>()
-    private val borrowedBooks = mutableSetOf<String>()
+    private val borrowedBooks = mutableMapOf<String, String>()
     private val borrowerFines = mutableMapOf<String, Int>()
 
-    fun addBook(book: Book) {
-        books[book.isbn] = book
-    }
+    fun addBook(book: Book) { books[book.isbn] = book }
 
     fun borrowBook(isbn: String, borrower: String) {
-        if (borrowedBooks.contains(isbn)) {
-            return
-        }
-        borrowedBooks.add(isbn)
+        if (isBorrowed(isbn) || !isAvailable(isbn)) throw IllegalArgumentException("Book $isbn is not available")
+        if (hasFines(borrower) || borrower in borrowedBooks.values) throw IllegalArgumentException("The borrower $borrower has a borrowed book or an outstanding debt")
+        borrowedBooks[isbn] = borrower
     }
 
     fun returnBook(isbn: String) {
+        if (!isExist(isbn)) throw IllegalArgumentException("Book $isbn is not exist in the library")
+        if (!isBorrowed(isbn)) throw IllegalArgumentException("Book $isbn is not borrowed")
         borrowedBooks.remove(isbn)
     }
 
-    fun isAvailable(isbn: String): Boolean {
-        return !borrowedBooks.contains(isbn)
-    }
+    fun isAvailable(isbn: String): Boolean = !isBorrowed(isbn) && isExist(isbn)
 
     fun calculateOverdueFine(isbn: String, daysOverdue: Int): Int {
-        if (!borrowedBooks.contains(isbn)) {
-            return 0
-        }
-        return daysOverdue * 60
+        if (!isBorrowed(isbn)) return 0
+        return max(daysOverdue-10, 0) * 60
     }
 
-    private fun hasOutstandingFines(borrower: String): Boolean {
-        return (borrowerFines[borrower] ?: 0) > 0
-    }
+    private fun isBorrowed(isbn: String): Boolean = borrowedBooks.containsKey(isbn)
+
+    private fun isExist (isbn: String): Boolean = books.containsKey(isbn)
+
+    private fun hasFines(borrower: String): Boolean = (borrowerFines[borrower] ?: 0) > 0
 }
